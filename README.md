89fbbf3cf112b00424378493bc1f0677a8bc1740
Lena <elena.mikhaylova0@gmail.com>
2025-09-23 16:03:03 -0400

    nix flake WIP

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# valence-zk-vault-template
+# Valence Vault ZK Template
+
+## Getting started
+1. Install prerequisites
+- [NixOs](https://nixos.org/download/)
+
+2. Start the development environment (the first time may take a few minutes):
+   ```bash
+   nix develop
+   ```
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Valence ZK Vault Template Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+          pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        nodejs = pkgs.nodejs_20;
+        npm = pkgs.nodePackages.npm;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+            buildInputs = [
+            nodejs
+            npm
+            pkgs.git
+        #  foundry
+          ];
+
+          shellHook = ''
+            export PS1="\[\033[1;36m\]Dev Shell \w \$\[\033[0m\]" # special chars change the color of the prompt
+            echo "🚀 Valence ZK Vault Template Development Environment"
+            echo "📦 Node.js version: $(node --version)"
+            echo "📦 npm version: $(npm --version)"
+            echo "🔨 Foundry version: $(forge --version)"
+            echo "⚡ Anvil available: $(anvil --version)"
+            echo ""
+          '';
+        };
+      });
+}
